68b2eb4053bd59674c896c955506dbfa77672423
do-thy <200067746+do-thy@users.noreply.github.com>
2026-05-14 20:43:47 +0800

    Change the firebase.ts values to be variables and not the actual values

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY ?? "AIzaSyBkMh0QojYD4AlWvHQz4XBUScnuEh6CQ6k",
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "arise-firebase-database.firebaseapp.com",
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL ?? "https://arise-firebase-database-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID ?? "arise-firebase-database",
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "arise-firebase-database.firebasedatabase.app",
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "403922722835",
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID ?? "1:403922722835:web:1f836add1c93ffc05722d6"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // initialize Firebase
